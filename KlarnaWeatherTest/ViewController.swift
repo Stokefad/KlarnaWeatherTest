a3b0140bd2f1b260a7f40de03c27d04194da60f7
Igor Naumenko <igor.naum.2013@yandex.ru>
2023-08-27 15:57:13 +0300

    Initial Commit

---
+++ KlarnaWeatherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KlarnaWeatherTest
+//
+//  Created by Igor Naumenko on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
